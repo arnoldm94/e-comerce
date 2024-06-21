cfb131b44e0ab8c12ef46a2ddfae2785d9b7ec50
kepapujana <kepapujana20@gmail.com>
2024-06-21 20:01:49 +0200

    db created

---
+++ models/productos.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Productos extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Productos.hasMany(models.Post)
+    }
+  }
+  Productos.init({
+    name: DataTypes.STRING,
+    price: DataTypes.FLOAT,
+    CategoriasId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Productos',
+  });
+  return Productos;
+};
