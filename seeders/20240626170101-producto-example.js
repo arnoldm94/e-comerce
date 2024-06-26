8c05c542e4b767d1173cb3500f91a8dcfaf349b3
Arnold Melet <arnoldmelet18@gmail.com>
2024-06-26 19:07:35 +0200

    first seeder

---
+++ seeders/20240626170101-producto-example.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return queryInterface.bulkInsert("Productos", [
+      {
+        name: "coches hot wheels",
+        price: "8",
+        CategoriasId: "1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
